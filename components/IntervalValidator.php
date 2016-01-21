72f9dfc3b1b260266d777f7d87327385d6d81fc6
gpouilly <gpouilly@10.100.40.43>
2016-01-21 16:51:29 +0100

    Création de la campagne unadev fidelisation

--- components/IntervalValidator.php
+++ components/IntervalValidator.php
@@ -15,22 +15,18 @@ class IntervalValidator extends Validator {
     }
 
     public function validateAttribute($model, $attribute) {
-        Yii::info('valida ' . $model->scenario, 'trace');
-        if ($model->scenario == 'PAM SLIMPAY') {
-            Yii::info('validadddd', 'trace');
-            $value = $model->$attribute;
+        Yii::info('validadddd', 'trace');
+        $value = $model->$attribute;
 
-            $datedujour = new \DateTime(Date('Y-m-d'));
-            $datenpa = \DateTime::createFromFormat('d/m/Y', $value);
+        $datedujour = new \DateTime(Date('Y-m-d'));
+        $datenpa = \DateTime::createFromFormat('d/m/Y', $value);
 
-            $diff = ($datenpa < $datedujour) ? -1 * ($datenpa->diff($datedujour)->format("%a")) : $datenpa->diff($datedujour)->format("%a");
+        $diff = ($datenpa < $datedujour) ? -1 * ($datenpa->diff($datedujour)->format("%a")) : $datenpa->diff($datedujour)->format("%a");
 
 
-            Yii::info($diff, 'trace');
-            if ($diff <= 10) {
-                $model->addError('N_DATEPA_MONTH', $this->message);
-                $model->addError('N_DATEPA_YEAR', $this->message);
-            }
+        if ($diff <= 10) {
+            $model->addError('N_DATEPA_MONTH', $this->message);
+            $model->addError('N_DATEPA_YEAR', $this->message);
         }
     }
 
